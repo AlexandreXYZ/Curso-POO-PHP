b2915cec069d9c6f792e6a9bed715d6e9aca2450
Alexandre Costa Belettini <61050276+AlexandreXYZ@users.noreply.github.com>
2021-02-02 13:18:43 -0300

    Aula_2_Completa
    Nesse codigo eu criei um modelo de uma caneta que pode ser usado no site por meio de um objetos.

---
+++ Aula02/Caneta.php
@@ -0,0 +1,30 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Caneta
+ *
+ * @author Alexandre XYZ
+ */
+class Caneta {
+    var $modelo;
+    var $cor;
+    var $ponta;
+    var $carga;
+    var $tampa;
+    
+    function rabiscar() {
+        echo "<p>A caneta está rabiscando</p>";
+    }
+    function tampar() {
+        
+    }
+    function destampar() {
+        
+    }
+}
