c7ef9557968926c4bfaeb8179c6b0a50ce8a0af2
Alexandre Costa Belettini <61050276+AlexandreXYZ@users.noreply.github.com>
2021-02-02 15:13:12 -0300

    Mundando para atalhos
    Erro no PHP

--- Aula09/Publicacao.php
+++
@@ -1,8 +0,0 @@
-<?php
-interface Publicacao {
-    public function abrir();
-    public function fechar();
-    public function folhear($p);
-    public function avançarPag();
-    public function voltarPag();
-}
